e91dbdb3e8f2adbc90609af9c1bb81e515a14abb
Scott Crawshaw <scott.crawshaw@gmail.com>
2020-11-20 20:21:51 -0500

    Final

--- get_doggo.py
+++ get_doggo.py
@@ -3,36 +3,50 @@ import requests
 import os.path
 from twilio.rest import Client
 
-response = requests.get('https://www.instagram.com/goldenunicornrae/?__a=1')
-profilePage = response.json()
-edges = profilePage["graphql"]['user']['edge_owner_to_timeline_media']['edges']
-ids=[]
-
-if os.path.isfile("dogIds.txt"):
-    f = open("dogIds.txt", "r")
-    for line in f:
-        ids.append(line.strip())
-    f.close()
-
-newIds = []
+accounts = ['goldenunicornrae', 'lizclimo', 'shutthekaleup', 'mattsurelee']
 media = []
 captions = []
-for edge in edges:
-    newIds.append(edge['node']['id'])
-    if edge['node']['id'] not in ids:
-        captions.append(edge['node']['edge_media_to_caption']['edges'][0]["node"]['text'])
-        if edge['node']["is_video"] == True:
-            postResponse = requests.get('https://www.instagram.com/goldenunicornrae/p/' + edge['node']['shortcode'] + '/?__a=1')
-            postJson = postResponse.json()
-            media.append('V' + postJson['graphql']['shortcode_media']['video_url'])
-        else:
-            media.append(edge['node']['display_url'])
 
-f = open("dogIds.txt", "w")
-for id in newIds:
-    f.write(id + "\n")
-f.close()
+for acct in accounts:
+    response = requests.get('https://www.instagram.com/'+acct+'/')
+    html = response.text
+    firstBit = 'window._sharedData = '
+    lastBit = ';</script>'
+    sharedData = html[html.index(firstBit) + len(firstBit) : html.index(lastBit)]
+    profilePage = json.loads(sharedData)['entry_data']['ProfilePage'][0]
+    edges = profilePage["graphql"]['user']['edge_owner_to_timeline_media']['edges']
+    ids=[]
+
+    if os.path.isfile(acct+"Ids.txt"):
+        f = open(acct+"Ids.txt", "r")
+        for line in f:
+            ids.append(line.strip())
+        f.close()
+
+    newIds = []
+    for edge in edges:
+        newIds.append(edge['node']['id'])
+        if edge['node']['id'] not in ids:
+            captions.append(edge['node']['edge_media_to_caption']['edges'][0]["node"]['text'])
+            if edge['node']["is_video"] == True:
+                postResponse = requests.get('https://www.instagram.com/'+acct+'/p/' + edge['node']['shortcode'] + '/')
+                html = postResponse.text
+                print(html)
+                firstBit = 'window._sharedData = '
+                lastBit = ';</script>'
+                sharedData = html[html.index(firstBit) + len(firstBit) : html.index(lastBit)]
+                postJson = json.loads(sharedData)['entry_data']['PostPage'][0]
+                media.append('V' + postJson['graphql']['shortcode_media']['video_url'])
+            else:
+                media.append(edge['node']['display_url'])
+
+    f = open(acct+"Ids.txt", "w")
+    for id in newIds:
+        f.write(id + "\n")
+    f.close()
+
 assert len(captions) == len(media)
+print(len(captions))
 account_sid = 'ACd8275d7e6a468abba1dbf8086d6e6259'
 auth_token = '01fc73e37c7c8e5ec33799c3d4df7415'
 client = Client(account_sid, auth_token)
